47c581ec631da3324cc1eb0197b52af13199fc0c
aujinsky <44969741+aujinsky@users.noreply.github.com>
2022-06-05 10:48:44 +0900

    Update constant_encoder.py

--- models/encoders/constant_encoder.py
+++ models/encoders/constant_encoder.py
@@ -1,14 +1,19 @@
 import torch
 from torch import nn
 
-
 class Encoder(nn.Module):
     def __init__(self, cfgmodel):
         super(Encoder, self).__init__()
         self.zdim = cfgmodel.zdim
-        self.out = nn.Parameter(torch.randn(1, self.zdim), requires_grad=True)
-
+        self.out = nn.Parameter(torch(randn(1, self.zdim), requires_grad=True))
     def forward(self, x):
-        bs = x.size(0)
-        m = v = self.out.expand(bs, -1)
-        return m, v
+        dim0 = x.shape[0]
+        return self.out.expand(dim0, -1), self.out.expand(dim0, -1)
+    
+if __name__ == "__main__":
+    class Cfg():
+        zdim = 512
+    enc = Encoder(Cfg)
+    m, v = enc.forward(torch.zeros(256))
+    print(enc.zdim, enc.out.shape)
+    print(m.shape, v.shape)

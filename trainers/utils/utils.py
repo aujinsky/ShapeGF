c4c0b9bc622cae0771d98904a769cdf54b48850b
aujinsky <44969741+aujinsky@users.noreply.github.com>
2022-06-06 23:15:24 +0900

    Update utils.py

--- trainers/utils/utils.py
+++ trainers/utils/utils.py
@@ -67,75 +67,70 @@ def set_random_seed(seed):
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-
 def ground_truth_field(prior_points, tr_pts, sigma):
+
+    # 3.2
+    # x_i = tr_pts
+    # x = prior_points
     bs, num_pts = tr_pts.size(0), tr_pts.size(1)
     smp_pts = prior_points.size(1)
     prior_points = prior_points.view(bs, smp_pts, 1, -1)
+    # bs, smp_pts, 1, dim
     tr_pts = tr_pts.view(bs, 1, num_pts, -1)
+    # bs, 1, num_pts, dim
     dist = (prior_points - tr_pts).norm(dim=3, keepdim=True) ** 2.
     a = - dist / sigma ** 2.
+    # argmin x_i x_i -x = argmax x_i x- x_i
     max_a, _ = torch.max(a, dim=2, keepdim=True)
     diff = torch.exp(a - max_a)
     w_i = diff / diff.sum(dim=2, keepdim=True)
-
+    # w_i = (bs, smp_pts, num_pts, 1)
+    # tr_pts = (bs, 1, num_pts, dim)
     # (bs, #pts-prior, 1, dim)
     trg_pts = (w_i * tr_pts).sum(dim=2, keepdim=True)
     y = - ((prior_points - trg_pts) / sigma ** 2.).view(bs, smp_pts, -1)
     return y
+    """
+    d2 = (prior_points-tr_pts).norm(dim=3, keepdim=True) ** 2.
+    ww_i = -1/(sigma**2)*d2 # difference between code and paper. we use coeff in repo.
+    w_i = ww_i / (ww_i.sum(dim=2, keepdim=True))
+    grad = 1/ (sigma**2) * (-prior_points + (w_i * tr_pts).sum(dim=2, keepdim=True))
+    grad = grad.view(bs, smp_pts, -1)
+    return grad
+    #doesn't works
+    """
 
-
-def ground_truth_reconstruct(inp, sigma, step_size, num_points=2048,
-                             num_steps=100, decay=1, interval=10, weight=1):
-    with torch.no_grad():
-        x = get_prior(inp.size(0), inp.size(1), inp.size(-1)).cuda()
-        x_list = []
-        x_list.append(x.clone())
-
-        for t in range(num_steps):
-            z_t = torch.randn_like(x) * weight
-            x += np.sqrt(step_size) * z_t
-            grad = ground_truth_field(x, inp, sigma)
-            x += 0.5 * step_size * grad
-            if t % (num_steps // interval) == 0:
-                step_size *= decay
-                x_list.append(x.clone())
-    return x, x_list
 
 
 def ground_truth_reconstruct_multi(inp, cfg):
+    # 3.4
     with torch.no_grad():
         assert hasattr(cfg, "inference")
         step_size_ratio = float(getattr(cfg.inference, "step_size_ratio", 1))
         num_steps = int(getattr(cfg.inference, "num_steps", 5))
         num_points = int(getattr(cfg.inference, "num_points", inp.size(1)))
         weight = float(getattr(cfg.inference, "weight", 1))
-
-        x = get_prior(
-            inp.size(0), num_points, cfg.models.scorenet.dim).cuda()
-        if hasattr(cfg.trainer, "sigmas"):
-            sigmas = cfg.trainer.sigmas
-        else:
-            sigma_begin = float(cfg.trainer.sigma_begin)
-            sigma_end = float(cfg.trainer.sigma_end)
-            num_classes = int(cfg.trainer.sigma_num)
-            sigmas = np.exp(np.linspace(np.log(sigma_begin), np.log(sigma_end),
+        sigma_begin = float(cfg.trainer.sigma_begin)
+        sigma_end = float(cfg.trainer.sigma_end)
+        num_classes = int(cfg.trainer.sigma_num) # k
+        sigmas = np.exp(np.linspace(np.log(sigma_begin), np.log(sigma_end),
                                         num_classes))
+        x = get_prior(inp.size(0), num_points, cfg.models.scorenet.dim).cuda()
+        # reused argument getting part, sigma generating part, prior generating part
+        # grad_ascent
         x_list = []
         x_list.append(x.clone())
-        bs, num_pts = x.size(0), x.size(1)
 
         for sigma in sigmas:
-            sigma = torch.ones((1,)) * sigma
-            sigma = sigma.cuda()
+            sigma = torch.full((1,), sigma).cuda()
             step_size = 2 * sigma ** 2 * step_size_ratio
             for t in range(num_steps):
-                z_t = torch.randn_like(x) * weight
-                x += torch.sqrt(step_size) * z_t
-                grad = ground_truth_field(x, inp, sigma)
-                x += 0.5 * step_size * grad
+                eps = torch.randn_like(x) * weight # in paper weight is always 1
+                x = x + step_size/2* ground_truth_field(x, inp, sigma) + torch.sqrt(step_size)*eps
             x_list.append(x.clone())
     return x, x_list
+
+
 
 
 def get_prior(batch_size, num_points, inp_dim):
